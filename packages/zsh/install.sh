0a993639e95aa4b1126af159779979ce176d0b3d
vitro <vitro@hackisfun.local>
2016-04-24 23:38:47 +0900

    zsh のインストーラー修正

--- packages/zsh/install.sh
+++ packages/zsh/install.sh
@@ -8,6 +8,9 @@ SRC="$HOME/src/osx-setup"
     cd $SRC && git pull
 }
 
+[ -d ~/.zsh_user ] && rm ~/.zsh_user
+[ -d ~/.zsh ] && rm ~/.zsh
+
 ln -sfn $SRC/packages/zsh/zsh ~/.zsh
 ln -sfn $SRC/packages/zsh/zsh_user ~/.zsh_user
 ln -sfn $SRC/packages/zsh/zshrc ~/.zshrc
