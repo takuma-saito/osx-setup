ed22772a53dc03b19c26f735d6af78ba9c06201c
vitro <vitro@hackisfun.local>
2016-04-25 08:28:11 +0900

    Mac デフォルトの環境設定を行う

---
+++ packages/init/defaults.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# 隠しファイルを可視化
+defaults write com.apple.finder AppleShowAllFiles true
+killall Finder
+
+# 拡張子を表示させる
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# ドックを左に変更する
+defaults write com.apple.dock orientation -string "left"
+
+# ドックを隠す
+defaults write com.apple.dock autohide -bool true
+
+# keyRepeat を最小にする
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# 副ボタンを右下の隅でクリックにする
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick 2
+
+# スクリーンを閉じたらパスワードを聞く
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
