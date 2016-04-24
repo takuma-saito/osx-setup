951bc8c5a7a83998db5eca48581059b4edfdefeb
taku <taku@takus-MacBook-Pro.local>
2016-04-24 20:03:16 +0900

    ファイル名変更

--- packages/emacs/install.sh
+++ packages/emacs/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mv -f ~/.emacs.d ~/.trash
 git clone https://github.com/takuma-saito/dot-emacs ~/.emacs.d
+cd ~/.emacs.d && cask install --verbose
 ln -sfn $HOME/.emacs.d/emacs-init.el $HOME/.emacs.el
--- packages/emacs/install/list-packages.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-ls -1d $1/*(/)  | tr '-' ' ' | rev | cut -d' ' -f2- | rev | tr ' ' '-'
