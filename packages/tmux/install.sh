56b803330bbc35ae1f4e55abfe585225aa7d0635
vitro <vitro@hackisfun.local>
2016-04-24 23:44:14 +0900

    tmux の設定追加

---
+++ packages/tmux/install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+SRC="$HOME/src/osx-setup"
+
+[ ! -d $SRC ] && {    
+    mkdir -p $SRC
+    git clone https://github.com/takuma-saito/osx-setup $SRC
+} || {
+    cd $SRC && git pull
+}
+
+[ -d ~/.tmux-session ] && rm ~/.tmux-session
+
+ln -sfn $SRC/packages/tmux/tmux.conf ~/.tmux.conf
+ln -sfn $SRC/packages/tmux/tmux-session ~/.tmux-session
