399a35f556ccb53ebd667d3b63dcdfc72a4b3c84
vitro <vitro@hackisfun.local>
2016-04-24 19:21:41 +0900

    karabiner のインストーラーに必要な設定追加

--- installer/karabiner.sh
+++ installer/karabiner.sh
@@ -3,12 +3,26 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set repeat.wait 20
+$cli set parameter.simultaneouskeypresses_pointingbutton_delay 20
 /bin/echo -n .
 $cli set repeat.initial_wait 200
 /bin/echo -n .
+$cli set parameter.simultaneouskeypresses_delay 20
+/bin/echo -n .
+$cli set parameter.doublepressmodifier_threshold 100
+/bin/echo -n .
 $cli set repeat.consumer_initial_wait 50
 /bin/echo -n .
+$cli set repeat.wait 30
+/bin/echo -n .
 $cli set repeat.consumer_wait 50
+/bin/echo -n .
+$cli set repeat.keyoverlaidmodifier_initial_wait 200
+/bin/echo -n .
+$cli set parameter.keyoverlaidmodifier_timeout 500
+/bin/echo -n .
+$cli set parameter.holdingkeytokey_wait 150
+/bin/echo -n .
+$cli set pointing.relative2scroll_rate 50
 /bin/echo -n .
 /bin/echo
