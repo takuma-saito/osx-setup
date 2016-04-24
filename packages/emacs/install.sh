10cd4b221a2c61745344038dd86d16a6551acf9e
vitro <vitro@hackisfun.local>
2016-04-24 21:30:27 +0900

    毎回 .emacs.d を削除いないようにする

--- packages/emacs/install.sh
+++ packages/emacs/install.sh
@@ -1,15 +1,29 @@
 #!/bin/zsh
 DIR="$HOME/src/osx-setup/packages/emacs"
 SRC="$HOME/src/osx-setup"
+CLEAN=
 
-[ -d $HOME/.emacs.d ] &&
+while getopts f OPT
+do
+    case $OPT in
+        c) CLEAN=true
+           ;;
+    esac
+done
+
+function cask_install() {    
+    cd $HOME/.emacs.d && cask install --verbose
+}
+
+[ $CLEAN ] && {
     mkdir -p $HOME/.trash/.emacs.d &&
-    rsync -a $HOME/.emacs.d $HOME/.trash/.emacs.d &&
-    rm -rf $HOME/.emacs.d
+        rsync -a $HOME/.emacs.d $HOME/.trash/.emacs.d &&
+        rm -rf $HOME/.emacs.d
+}
 mkdir -p $SRC
 [ ! -d $SRC ] &&
     git clone https://github.com/takuma-saito/osx-setup $SRC || { cd $SRC && git pull }
-cp -a $SRC/packages/emacs $HOME/.emacs.d &&
-    cd $HOME/.emacs.d &&
-    cask install --verbose
+[ -d $HOME/.emacs.d ] && cask_install || {
+    cp -a $SRC/packages/emacs $HOME/.emacs.d && cask_install
+}
 ln -sfn $HOME/.emacs.d/init.el $HOME/.emacs.el
