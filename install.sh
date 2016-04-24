d04b37ea437e301f3c2eba0b7d015019baddb345
vitro <vitro@hackisfun.local>
2016-04-24 23:51:44 +0900

    brew install で読み込むファイルをダウンロードしたレポジトリから読み込むように修正

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./packages/brew/install.sh
+./packages/emacs/install.sh
+./packages/karabiner/install.sh
+./packages/tmux/install.sh
+./packages/zsh/install.sh
--- packages/brew/install.sh
+++ packages/brew/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+SRC="$HOME/src/osx-setup"
+
+[ ! -d $SRC ] && {    
+    mkdir -p $SRC
+    git clone https://github.com/takuma-saito/osx-setup $SRC
+} || {
+    cd $SRC && git pull
+}
 
 brew update
 brew tap sanemat/font
 brew tap caskroom/cask
 
-cat brew-list.txt | xargs -I{} brew install -v {}
-cat brew-cask-list.txt | xargs -I{} brew cask install -v --appdir=/Applications {}
+cat $SRC/packages/brew/brew-list.txt | xargs -I{} brew install -v {}
+cat $SRC/packages/brew/brew-cask-list.txt | xargs -I{} brew cask install -v --appdir=/Applications {}
