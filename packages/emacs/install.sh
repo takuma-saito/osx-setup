458abc7ab65f34956b584c7c0357b8d52f54169f
vitro <vitro@hackisfun.local>
2016-04-24 21:19:45 +0900

    バグ修正

--- packages/emacs/install.sh
+++ packages/emacs/install.sh
@@ -4,7 +4,7 @@ SRC="$HOME/src/osx-setup"
 
 [ -d $HOME/.emacs.d ] &&
     mkdir -p $HOME/.trash/.emacs.d &&
-    rsync -a $HOME/.emacs.d $HOME/.trash/ &&
+    rsync -a $HOME/.emacs.d $HOME/.trash/.emacs.d &&
     rm -rf $HOME/.emacs.d
 mkdir -p $SRC
 [ ! -d $SRC ] &&
