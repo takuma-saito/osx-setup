7066af596b5bfa1cf82242fa6c8bbd1d4dd9f364
vitro <vitro@hackisfun.local>
2016-04-24 21:03:31 +0900

    emacs のインストーラー修正

--- packages/emacs/install.sh
+++ packages/emacs/install.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
-mv -f ~/.emacs.d ~/.trash
-git clone https://github.com/takuma-saito/dot-emacs ~/.emacs.d && cd ~/.emacs.d && cask install --verbose
+#!/bin/zsh
+DIR="$HOME/src/osx-setup/packages/emacs"
+SRC="$HOME/src/osx-setup"
+
+[ -d $HOME/.emacs.d ] &&
+    mkdir -p $HOME/.trash/.emacs.d &&
+    rsync -a $HOME/.emacs.d $HOME/.trash/ &&
+    rm -rf $HOME/.emacs.d
+mkdir -p $SRC
+rm -rf $HOME/src/osx-setup &&
+    git clone https://github.com/takuma-saito/osx-setup $SRC &&
+    cp -a $SRC/packages/emacs $HOME/.emacs.d
+cd $HOME/.emacs.d && cask install --verbose
 ln -sfn $HOME/.emacs.d/emacs-init.el $HOME/.emacs.el
