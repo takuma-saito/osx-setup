dcdd8682dd3601379154406edf9d4e938370baa7
vitro <vitro@hackisfun.local>
2016-04-24 20:05:26 +0900

    installer 微修正

--- packages/emacs/install.sh
+++ packages/emacs/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mv -f ~/.emacs.d ~/.trash
-git clone https://github.com/takuma-saito/dot-emacs ~/.emacs.d
-cd ~/.emacs.d && cask install --verbose
+git clone https://github.com/takuma-saito/dot-emacs ~/.emacs.d && cd ~/.emacs.d && cask install --verbose
 ln -sfn $HOME/.emacs.d/emacs-init.el $HOME/.emacs.el
