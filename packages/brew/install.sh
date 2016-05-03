dce09c521d82c1c71a9000622be3509c3d79ca7a
vitro <vitro@hackisfun.local>
2016-05-03 20:25:15 +0900

    installer を https => git+ssh に変更

--- packages/brew/install.sh
+++ packages/brew/install.sh
@@ -3,7 +3,7 @@ SRC="$HOME/src/osx-setup"
 
 [ ! -d $SRC ] && {    
     mkdir -p $SRC
-    git clone https://github.com/takuma-saito/osx-setup $SRC
+    git clone git+ssh://git@github.com/takuma-saito/osx-setup $SRC
 } || {
     cd $SRC && git pull
 }
