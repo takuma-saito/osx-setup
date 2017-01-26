b18ad746d160d7125875d4f828cd0e6ede89d505
takuma.saito <takuma.saito@theport.jp>
2017-01-26 17:13:20 +0900

    json が正しいモードで閲覧できるようにした

--- packages/emacs/init/lang.el
+++ packages/emacs/init/lang.el
@@ -57,7 +57,7 @@
 
 ;;; js2-mode
 (autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.\\(js\\|json\\)$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.pac$" . js2-mode))
 
 ;; css の設定
