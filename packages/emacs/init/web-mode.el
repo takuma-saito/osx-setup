dfc9f9e30dda6c1efda4b4f2a10e550fa20d2109
takuma.saito <takuma.saito@theport.jp>
2017-01-26 16:53:24 +0900

    add .ejs

--- packages/emacs/init/web-mode.el
+++ packages/emacs/init/web-mode.el
@@ -14,6 +14,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs$"       . web-mode))
 
 ;;; インデント数
 (defun web-mode-hook ()
