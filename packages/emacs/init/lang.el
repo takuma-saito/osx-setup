4303e0f6ba0af2eff7b82b23baa7fd86e155acaa
takuma.saito <takuma.saito@theport.jp>
2017-03-22 18:54:48 +0900

    add emmet

--- packages/emacs/init/lang.el
+++ packages/emacs/init/lang.el
@@ -124,4 +124,10 @@
             (setq indent-level 4)
             (setq python-indent 4)))
 
+;; emmet
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
+(setq emmet-move-cursor-after-expanding nil)
 
