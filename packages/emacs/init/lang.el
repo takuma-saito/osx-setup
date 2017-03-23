6968e77eee12b141e888ea52bae98f77c6380497
takuma.saito <takuma.saito@theport.jp>
2017-03-23 12:56:16 +0900

    update lang

--- packages/emacs/init/lang.el
+++ packages/emacs/init/lang.el
@@ -126,8 +126,10 @@
 
 ;; emmet
 (require 'emmet-mode)
+(add-hook 'web-mode-hook  'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
+(setq emmet-self-closing-tag-style "")
 (setq emmet-move-cursor-after-expanding nil)
 
