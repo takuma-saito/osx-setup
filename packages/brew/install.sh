19307c1cf4cbeace1669ab24a9a1cd6c9b0ef4d4
TAKUMA SAITO <takumasaito@TAKUMAnoMacBook-puro.local>
2018-07-09 00:03:19 +0900

    from ssh to https

--- packages/brew/install.sh
+++ packages/brew/install.sh
@@ -3,7 +3,7 @@ SRC="$HOME/src/osx-setup"
 
 [ ! -d $SRC ] && {    
     mkdir -p $SRC
-    git clone git@github.com/takuma-saito/osx-setup $SRC
+    git clone https://github.com/takuma-saito/osx-setup.git $SRC
 } || {
     cd $SRC && git pull
 }
