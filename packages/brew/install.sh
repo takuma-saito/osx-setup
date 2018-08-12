143d4923562a57986f10a0811a649e76462b9f07
t.saito <takuma.saitou1@gmail.com>
2018-08-12 15:36:17 +0900

    全体的に調整した

--- packages/brew/install.sh
+++ packages/brew/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -xe
 # brew のインストール
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew tap sanemat/font
-brew tap caskroom/cask
-brew tap caskroom/versions
-brew tap buo/cask-upgrade
-brew update --verbose
+#brew tap sanemat/font
+#brew tap caskroom/cask
+#brew tap caskroom/versions
+#brew tap buo/cask-upgrade
+#brew update --verbose
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew install --force --verbose $(cat ./packages/brew/brew-list.txt)
-brew cask install --verbose --appdir=/Applications --force $(cat ./packages/brew/brew-cask-list.txt)
+#brew cask install --verbose --appdir=/Applications --force $(cat ./packages/brew/brew-cask-list.txt)
 brew install http://git.io/sshpass.rb
 
 # install ricty
